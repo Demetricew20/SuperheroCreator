af066490ee45fa1f0eac95cb29d3c01c0b628de8
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-06 18:18:04 -0800

    Add functionality for create superhero form button

--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
 
 
@@ -18,3 +20,17 @@ def detail(request, superhero_id):
     }
 
     return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        hero_name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(hero_name=hero_name, alter_ego=alter_ego, primary_ability=primary_ability,
+                                  secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
