a0daa3e590b6c50b88bc4886439cabf3df0352cd
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-06 18:10:45 -0800

    Created create.html for superhero creation page. Add url to urls.py in app. Created form in create.html.

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -5,5 +5,6 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:superhero_id>/', views.detail, name='detail'),
+    path('create/', views.create, name='create_superhero'),
 
 ]
